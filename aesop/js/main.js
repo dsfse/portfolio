1f3383cbd25e30bed48fa58b4296721614021b40
dsfse <sangmin1465@gmail.com>
2023-05-09 20:49:20 +0900

    s

--- aesop/js/main.js
+++ aesop/js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
-    const swiper = new Swiper('.perfume .list', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+    const swiper2 = new Swiper('.event .list', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-        centeredSlides: true,
-        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
         breakpoints: {
-            700: {    /* 640px 이상일때 적용 */
+            640: {    
+                slidesPerView: 2,
                 spaceBetween: 30,
             },
-            850: {    /* 1200px 이상일때 적용 */
-            spaceBetween: 40,
-            },
-            1440: {    /* 1440px 이상일때 적용 */
-            spaceBetween: 40,
+            1000: {   
+                slidesPerView: 3,
+                spaceBetween: 40,
             },
         },
     });
